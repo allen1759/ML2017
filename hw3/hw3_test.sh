9f6a587b35a0c30fa81779b3494054bb7e53d5f2
Allen Yang <allen1759@gmail.com>
2017-05-10 18:51:02 +0800

    Update download address of model.

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -1,3 +1,4 @@
 
-wget https://www.dropbox.com/s/hh8hqtmmxrunu54/model.h5?dl=0 -O model.h5
+# wget https://www.dropbox.com/s/hh8hqtmmxrunu54/model.h5?dl=0 -O model.h5
+wget https://github.com/allen1759/ML2017/releases/download/v0.0/model.h5 -O model.h5
 python hw3_test.py $1 $2
